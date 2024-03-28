d6b680c50388bb0548c0283ab067ea43fc7b4baa
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-03-29 00:00:58 +0200

    feat: created sign in api route

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import User from './../model/user.model.js';
 import bcryptjs from "bcryptjs";
+import Jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 
 export const signUp = async (req, res, next) => {
    const { username, email, password } = req.body;
@@ -15,4 +17,38 @@ export const signUp = async (req, res, next) => {
    } catch (error) {
         next(error);
    }
+};
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({email}); 
+
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'));
+        }
+
+        const validUserPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validUserPassword) {
+            return new(errorHandler(401, 'Wrong credentials!'));
+        }
+
+        // Set the jwtToken on the user id
+        const jwtToken = Jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+
+        const {password: pass, ...rest} = validUser._doc;
+
+        // Set the session cookie, to not allow 3-rd party reading cookie & expiration time
+        res.cookie('acces_token', jwtToken, {
+            httpOnly: true, 
+            expires: new Date(Date.now() + 24 * 60),
+        })
+        .status(200)
+        .json(rest)
+        ;
+    } catch (error) {
+        next(error);
+    }
 };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controller/auth.controller.js";
+import { signUp, signIn } from "../controller/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/signin", signIn);
 
 export default router;
