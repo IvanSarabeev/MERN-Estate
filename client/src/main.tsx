1cdd30950113ea46cde359e583c7fa5ae55c070c
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-03-30 16:41:23 +0200

    feat: persisting store state

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { store } from "store/store.ts";
+import { persistor, store } from "store/store.ts";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,14 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+    user: userSlice,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        user: userSlice
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }),
 })
+
+export const persistor = persistStore(store);
 
 // Infer the "RootState" and "AppDispatch" types from the store itself
 export type RootState = ReturnType<typeof store.getState>
