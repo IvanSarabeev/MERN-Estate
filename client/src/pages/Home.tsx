4dd93c7a91454fb73053ef68217d7760aa8f8da6
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-04-14 17:50:53 +0300

    style: home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -31,7 +31,7 @@ const Home: React.FC = () => {
 
   return (
     <Layout>
-      <section className="relative h-screen w-screen bg-home-wallpaper bg-cover bg-top aspect-auto">
+      {/* <section className="relative h-screen w-screen bg-home-wallpaper bg-cover bg-top aspect-auto">
         <div className="absolute left-[20%] top-1/4 gap-6 flex flex-col items-start justify-start p-16 rounded-2xl bg-slate-300">
           <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
             Find your next <span className="text-slate-500">perfect</span>
@@ -43,6 +43,32 @@ const Home: React.FC = () => {
           >
             Let's get started ...
           </Link>
+        </div>
+      </section> */}
+      <section className="padding-container bg-white">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl">
+              Find your next <span className="text-slate-500">perfect</span>
+              <br /> place with ease
+            </h1>
+            <p className="max-w-xl md:regular-16 xl:regular-18 mb-6 font-light text-gray-500 lg:mb-8">
+              From checkout to global sales tax compliance, companies around the
+              world use Flowbite to simplify their payment stack.
+            </p>
+            <Link
+              to={"/search"}
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 regular-12 sm:regular-14 lg:regular-16 text-blue-700 hover:underline transition-all ease-in-out hover:text-gray-600"
+            >
+              Let's get started ...
+            </Link>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+              alt="mockup"
+            />
+          </div>
         </div>
       </section>
       <section className="w-screen padding-container bg-white">
