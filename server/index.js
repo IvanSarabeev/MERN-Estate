8aa7977cf25eeffd80e96d3f577e66be1af3eb1b
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-04-14 22:26:55 +0300

    added pre server installation

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT_KEY;
@@ -18,6 +19,8 @@ mongoose.connect(DB_URL)
         console.log(error);
     });
     
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -33,6 +36,12 @@ app.listen(PORT, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
  
 app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500;
