620709158b03f554b64b46333816a01b5d31921d
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-06-25 22:44:48 +0300

    added input & completed layout

--- client/src/components/Layouts/AccountLayout.tsx
+++ client/src/components/Layouts/AccountLayout.tsx
@@ -1,62 +1,61 @@
 import React from "react";
-import {Link, Outlet} from "react-router-dom";
-import {
-    CircleUser,
-    Menu,
-    Package2,
-    Search
-} from "lucide-react";
+import { Link, Outlet } from "react-router-dom";
+import { CircleUser, Menu, Package2, Search } from "lucide-react";
 import { internalNavigation } from "../constants";
-import {Sheet, SheetContent, SheetTrigger} from "components/ui/sheet.tsx";
-import {Button} from "components/ui/button.tsx";
-import Input from "components/HTML/Input.tsx";
+import { Sheet, SheetContent, SheetTrigger } from "components/ui/sheet.tsx";
+import { Button } from "components/ui/button.tsx";
+import { Input } from "../ui/input";
 import {
   DropdownMenu,
-  DropdownMenuContent, DropdownMenuItem,
-  DropdownMenuLabel, DropdownMenuSeparator,
-  DropdownMenuTrigger
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "components/ui/dropdown-menu.tsx";
 import Footer from "components/Footer/Footer.tsx";
-
 
 const AccountLayout: React.FC = () => {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-        <nav
-            className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link
-              to="#"
-              className="flex items-center gap-2 text-lg font-semibold md:text-base"
+            to="#"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
           >
-            <Package2 className="size-6"/>
+            <Package2 className="size-6" />
             <span className="sr-only">Acme Inc</span>
           </Link>
           {internalNavigation.map((item) => {
             return (
-                <Link key={item.id} to={item.href}>
-                  {item.label}
-                </Link>
+              <Link key={item.id} to={item.href}>
+                {item.label}
+              </Link>
             );
           })}
           <Sheet>
             <SheetTrigger asChild>
-              <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-                <Menu className="size-5"/>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="size-5" />
                 <span className="sr-only">Toggle navigation menu</span>
               </Button>
             </SheetTrigger>
             <SheetContent side="left">
               <nav className="grid gap-6 regular-18">
                 <Link to="#" className="flex items-center gap-2 regular-18">
-                  <Package2 className="size-6"/>
+                  <Package2 className="size-6" />
                   <span className="sr-only">Acme Inc</span>
                 </Link>
                 {internalNavigation.map((item) => {
                   return (
-                      <Link key={item.id} to={item.href}>
-                        {item.label}
-                      </Link>
+                    <Link key={item.id} to={item.href}>
+                      {item.label}
+                    </Link>
                   );
                 })}
               </nav>
@@ -66,11 +65,11 @@ const AccountLayout: React.FC = () => {
         <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
           <form className="ml-auto flex-1 sm:flex-initial">
             <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 size-4 text-muted-foreground"/>
+              <Search className="absolute left-2.5 top-2.5 size-4 text-muted-foreground" />
               <Input
-                  type="search"
-                  placeholder="Search products..."
-                  className="py-1.5 px-2 rounded-md ml-2 outline-2 outline-gray-500 pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+                type="search"
+                placeholder="Search products..."
+                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
               />
             </div>
           </form>
@@ -92,12 +91,12 @@ const AccountLayout: React.FC = () => {
           </DropdownMenu>
         </div>
       </header>
-      <main>
+      <main className="account-container">
         <Outlet />
       </main>
       <Footer />
     </div>
-);
+  );
 };
 
 export default AccountLayout;
