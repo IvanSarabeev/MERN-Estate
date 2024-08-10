558d40e709110a2713fae9a941acd6094d4aa945
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-08-10 15:00:10 +0300

    fix: removed redux sign in

--- client/src/services/apiAuth.ts
+++ client/src/services/apiAuth.ts
@@ -1,8 +1,5 @@
-import { UserSignInData, UserSignUpData } from 'types/user';
-import { 
-    signInStart, 
-    signInSucces, 
-    signInFailure,
+import { UserSignUpData } from 'types/user';
+import {
     signOutUserStart,
     signOutUserSuccess,
     signOutUserFailure,
@@ -12,7 +9,6 @@ import { AppDispatch } from 'store/store';
 const postMethod = 'POST';
 
 const urlSignUp: string = "/api/auth/signup";
-const urlSignIn: string = "/api/auth/signin";
 const urlSignOut: string = "/api/auth/signout";
 
 export const registerUser = async (formData: UserSignUpData) => {
@@ -37,38 +33,6 @@ export const registerUser = async (formData: UserSignUpData) => {
     }
 };
 
-export const signInUser = async (formData: UserSignInData, dispatch: AppDispatch) => {
-    try {
-        dispatch(signInStart());
-
-        const response = await fetch(urlSignIn, {
-            method: postMethod,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
-        
-        if (!response.ok) {
-            const errorData = await response.json();
-
-            throw new Error(`Error ${response.status}: ${errorData.message || response.statusText}`);
-        }
-        
-        const data = await response.json();
-
-        if (data === false) {
-            return dispatch(signInFailure(data));
-        }
-
-        dispatch(signInSucces(data));
-        
-        return data;
-    } catch (error) {
-        dispatch(signInFailure(error));
-        throw new Error(`Error message: ${JSON.stringify(error)}`)
-    }
-}
 
 export const signOutUser = async (dispatch: AppDispatch) => {
     try {
